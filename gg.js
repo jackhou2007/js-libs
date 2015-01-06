b81f9f662cb841b7f6d3dcc41d6fd145225adac4
jackhou2007 <jackhou2007@gmail.com>
2015-01-07 00:04:36 +0800

    update

--- gg.js
+++ gg.js
@@ -4,6 +4,37 @@
  */
 ;(function (window, document, undefined) {
   var gg = {};
+  gg.$ = function (selector, element) {
+    if (!element) {
+      element = document;
+    }
+
+    try {
+      return element.querySelector(selector);
+    } catch (e) {
+
+    }
+  };
+
+  gg.$$ = function (selector, element) {
+    if (!element) {
+      element = document;
+    }
+
+    try {
+      return element.querySelectorAll(selector);
+    } catch (e) {
+
+    }
+  };
+
+  gg.id = function (selector, element) {
+    if (!element) {
+      element = document;
+    }
+
+    return element.getElementById(selector);
+  };
 
   window.gg = gg;
 })(window, document);
