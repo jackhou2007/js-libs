b26d81a0dbdc840c5013c1c53650e0bcabaa3a0b
jackhou2007 <jackhou2007@gmail.com>
2015-01-07 00:15:55 +0800

    update

--- gg.js
+++ gg.js
@@ -36,5 +36,43 @@
     return element.getElementById(selector);
   };
 
+  gg.getChildren = function (selector, element) {
+    var parent = this.$(selector, element);
+    return parent.childNodes;
+  };
+
+  /**
+   * DOM
+   */
+  gg.dom = gg.dom || {};
+  gg.dom.isElement = function (element) {
+    return element && element.nodeType === Node.ELEMENT_NODE;
+  }
+
+  gg.dom.firstChild = function (element) {
+    return isElement(element) ? element.firstChild : null;
+  }
+
+  /**
+   * tools (include extend grammer)
+   * @type {[type]}
+   */
+  gg.util = gg.util || {};
+  gg.util.isArray = function (obj) {
+    return Object.protype.toString.call(obj) === '[Object Array]';
+  };
+
+  gg.util.makeArray = function (obj) {
+    return Array.prototype.slice.call(obj);
+  }
+
+  gg.util.trim = function (string) {
+    return string.replace(/^\s|\s$/g, '');
+  };
+
+  gg.util.extend = function () {
+
+  };
+
   window.gg = gg;
 })(window, document);
