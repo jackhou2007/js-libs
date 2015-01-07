79bfbdc5d07838a83bfb5f142d3681009a08aca2
jackhou2007 <jackhou2007@gmail.com>
2015-01-07 09:33:23 +0800

    add ajax file

---
+++ ajax.js
@@ -0,0 +1,17 @@
+/**
+ * @date 2015-01-07
+ * @author jialin.hou@raidtalk.com
+ */
+;(function (window, document, undefined) {
+  var xhr,
+    onComplete,
+    onSuccess,
+    onError;
+
+  function ajax(options) {
+    xhr = new XMLHttpRequest();
+    window.util.extend({}, this.defaults, options);
+  }
+
+  window.ajax = ajax;
+}).call(this);
